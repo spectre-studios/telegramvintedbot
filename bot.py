e7ec3f6df5254416fe5f9a98b3d69db9e367a9f7
spectre <spectredesignstudio@protonmail.com>
2026-09-19 14:54:54 +0200

    proxy pretest v2

--- bot.py
+++ bot.py
@@ -109,7 +109,10 @@ vinted_cookie = os.getenv("VINTED_COOKIE", "")
 vinted_client = VintedScraper("https://www.vinted.fr")
 scraper = VintedScraper(
     ("https://www.vinted.fr"),
-    cookie=vinted_cookie if vinted_cookie else None
+    headers={
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Cookie": f"access_token_web={vinted_cookie}" if vinted_cookie else ""
+    }
 )
 
                         
